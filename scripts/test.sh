331f47e02f3f7203626356084b8d88171728fe9c
hazelnutsgz <hazelnutsgz@gmail.com>
2022-03-01 10:42:28 -0500

    update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,5 +3,6 @@
 ./gradlew build || exit 1
 ./gradlew cloverAggregateReport || exit 1
 scripts/coverage_summary.sh
-cp -r app/build/reports/clover/html/* /coverage-out/ || exit 1
-
+cp -r server/build/reports/clover/html/* /coverage-out/ || exit 1
+cp -r client/build/reports/clover/html/* /coverage-out/ || exit 1
+cp -r common/build/reports/clover/html/* /coverage-out/ || exit 1
