98d09a911d0bde1783e0da3a6a6b8a3d3030a037
Tianyi <tc255@duke.edu>
2022-03-03 16:23:10 -0500

    Simple Map

--- common/src/main/java/team5/risc/common/Map.java
+++ common/src/main/java/team5/risc/common/Map.java
@@ -1,12 +1,25 @@
 package team5.risc.common;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Map implements Serializable {
-    private static final long serialVersionUID = 1L;
-    public int num_players;
-    public int num_territories;
-    public Map(int players, int t) {
-        this.num_players = players;
-        this.num_territories = t;
+  private static final long serialVersionUID = 1L;
+  public ArrayList<AreaNode> areas;
+
+  public Map(int num_area) {
+    this.areas = new ArrayList<AreaNode>();
+    for (int i = 0; i < num_area; i++) {
+      this.areas.add(new AreaNode("area1"));
     }
+  }
+
+  public ArrayList<String> getAreasName() {
+    ArrayList<String> areas_name = new ArrayList<String>();
+    for (int i = 0; i < areas.size(); i++) {
+      areas_name.add(areas.get(i).getName());
+    }
+    return areas_name;
+  }
 }
---
+++ common/src/test/java/team5/risc/common/MapTest.java
@@ -0,0 +1,15 @@
+package team5.risc.common;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MapTest {
+  @Test
+  public void test_Map() {
+    Map m = new Map(1);
+    assertEquals("area1", m.getAreasName().get(0));
+
+  }
+
+}
