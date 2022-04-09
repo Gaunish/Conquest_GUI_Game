846c308861d44a27ae66dbd9a58baa32d157fdb4
Gaunish <anigarg1999@gmail.com>
2022-04-09 16:58:59 -0400

    Robot

---
+++ client/src/test/java/team5/risc/client/GUIClientTest.java
@@ -0,0 +1,49 @@
+package team5.risc.client;
+
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.net.URL;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
+
+public class GUIClientTest extends ApplicationTest {
+    @Override
+    public void start(Stage stage) throws Exception {
+        URL xmlResource = getClass().getResource("/ui/login.fxml");
+        FXMLLoader loader = new FXMLLoader(xmlResource);
+        StackPane gp = loader.load();
+        Scene scene = new Scene(gp, 640, 480);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        FxToolkit.hideStage();
+        release(new KeyCode[] {});
+        release(new MouseButton[] {});
+    }
+
+    @Test
+    public void testEnglishInput() {
+        clickOn("#Button");
+        Platform.exit();
+        System.exit(0);
+    }
+    
+}
